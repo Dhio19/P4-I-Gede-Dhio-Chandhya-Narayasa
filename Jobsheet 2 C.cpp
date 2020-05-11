a42427ff24f04c6aaf19581c98d3b1f2b900d346
Dhio19 <64562565+Dhio19@users.noreply.github.com>
2020-05-11 15:19:05 +0700

    Add files via upload
    Membuat program mengisi dan menampilkan elemen array dengan index array sesuai dengan NIM terakhir setiap Mahasiswa.

---
+++ Jobsheet 2 C.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+using namespace std;
+
+main(){
+	
+	printf("Nama : I Gede Dhio Chandhya Narayasa\nNIM : F1B019063");
+	int z [13];
+	printf("\nKelompok : 13\n");
+	printf("Masukkan nilai 1 = ");
+	scanf("%d",&z[0]);
+	printf("Masukkan nilai 2 = ");
+	scanf("%d",&z[1]);
+	printf("Masukkan nilai 3 = ");
+	scanf("%d",&z[2]);
+	printf("Masukkan nilai 4 = ");
+	scanf("%d",&z[3]);
+	printf("Masukkan nilai 5 = ");
+	scanf("%d",&z[4]);
+	printf("Masukkan nilai 6 = ");
+	scanf("%d",&z[5]);
+	printf("Masukkan nilai 7 = ");
+	scanf("%d",&z[6]);
+	printf("Masukkan nilai 8 = ");
+	scanf("%d",&z[7]);
+	printf("Masukkan nilai 9 = ");
+	scanf("%d",&z[8]);
+	printf("Masukkan nilai 10 = ");
+	scanf("%d",&z[9]);
+	printf("Masukkan nilai 11 = ");
+	scanf("%d",&z[10]);
+	printf("Masukkan nilai 12 = ");
+	scanf("%d",&z[11]);
+	printf("Masukkan nilai 13 = ");
+	scanf("%d",&z[12]);
+	
+	printf("\nData ke-1 = %d",z[0]);
+	printf("\nData ke-2 = %d",z[1]);
+	printf("\nData ke-3 = %d",z[2]);
+	printf("\nData ke-4 = %d",z[3]);
+	printf("\nData ke-5 = %d",z[4]);
+	printf("\nData ke-6 = %d",z[5]);
+	printf("\nData ke-7 = %d",z[6]);
+	printf("\nData ke-8 = %d",z[7]);
+	printf("\nData ke-9 = %d",z[8]);
+	printf("\nData ke-10 = %d",z[9]);
+	printf("\nData ke-11 = %d",z[10]);
+	printf("\nData ke-12 = %d",z[11]);
+	printf("\nData ke-13 = %d",z[12]);
+		
+}
+
