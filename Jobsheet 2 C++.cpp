2237691f0dc73fa980c64a4b9da0c5e27a5ef4b4
Dhio19 <64562565+Dhio19@users.noreply.github.com>
2020-05-11 15:20:43 +0700

    Add files via upload
    Membuat program mengisi dan menampilkan elemen array dengan index array sesuai dengan NIM terakhir setiap Mahasiswa.

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using  namespace std;
+
+int main(){
+	int z[13];
+	cout<<"Nama : I Gede Dhio Chandhya Narayasa\nNIM : F1B019063";
+	cout<<"\nKelompok : 13";
+	cout<<"\nMasukkan nilai 1 = ";
+	cin>>z[0];
+	cout<<"Masukkan nilai 2 = ";
+	cin>>z[1];
+	cout<<"Masukkan nilai 3 = ";
+	cin>>z[2];
+	cout<<"Masukkan nilai 4 = ";
+	cin>>z[3];
+	cout<<"Masukkan nilai 5 = ";
+	cin>>z[4];
+	cout<<"Masukkan nilai 6 = ";
+	cin>>z[5];
+	cout<<"Masukkan nilai 7 = ";
+	cin>>z[6];
+	cout<<"Masukkan nilai 8 = ";
+	cin>>z[7];
+	cout<<"Masukkan nilai 9 = ";
+	cin>>z[8];
+	cout<<"Masukkan nilai 10 = ";
+	cin>>z[9];
+	cout<<"Masukkan nilai 11 = ";
+	cin>>z[10];
+	cout<<"Masukkan nilai 12 = ";
+	cin>>z[11];
+	cout<<"Masukkan nilai 13 = ";
+	cin>>z[12];
+	cout<<"\n\nData ke-1 = "<<z[0];
+	cout<<"\nData ke-2 = "<<z[1];
+	cout<<"\nData ke-3 = "<<z[2];
+	cout<<"\nData ke-4 = "<<z[3];
+	cout<<"\nData ke-5 = "<<z[4];
+	cout<<"\nData ke-6 = "<<z[5];
+	cout<<"\nData ke-7 = "<<z[6];
+	cout<<"\nData ke-8 = "<<z[7];
+	cout<<"\nData ke-9 = "<<z[8];
+	cout<<"\nData ke-10 = "<<z[9];
+	cout<<"\nData ke-11 = "<<z[10];
+	cout<<"\nData ke-12 = "<<z[11];
+	cout<<"\nData ke-13 = "<<z[12];
+}
+
